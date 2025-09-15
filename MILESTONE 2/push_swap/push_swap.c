1a123b687e412d87cb27d2f680cbd3437fdc820f
unovo-ru <uli.nr.97@gmail.com>
2025-09-15 18:48:31 +0200

    hecho index y pos

--- MILESTONE 2/push_swap/push_swap.c
+++ MILESTONE 2/push_swap/push_swap.c
@@ -34,58 +34,21 @@ t_node	*push_swap(char **str)
 	if (is_correct(res, size) == 1)
 	{
 		stack = node_aplication(res, size);
-		print_node(stack);
-		printf("\t------STACK A------\n");
 		index_node(&stack, size);
 		position_node(stack);
-		print_node(stack);
-		printf("\t------STACK A------\n");
-		// select_hc(size, &stack);
-		// printf("####### STACK A #######");
-		// print_node(stack);
-		// printf("####### STACK B #######");
-		// print_node(stack_b);
 	}
 	else
 		free(res);
-	// if (is_correct(res, size) == 0)
-	// {
-	// 	printf("\n\nhola\n\n");
-	// 	free(final_argument);
-	// 	return (NULL);
-	// }
-	// else
-	// {
-	// 	select_hc(size, &stack);
-	// 	// is_min(&stack);
-	// 	// index_node(stack);
-	// 	printf("\t------STACK A------\n");
-	// 	print_node(stack);
-	// }
-	// printf("este es el input final = %s\n", final_argument);
-	// printf("este es el size = %d\n", size);
-	// if (!is_correct(stack))
-	// {
-	// 	printf("hola\n\n");
-	// 	free(stack);
-	// 	free(final_argument);
-	// 	return (NULL);
-	// }
-	//else
 	free(final_argument);
 	return (stack);
 }
 
+		// print_node(stack);
+		// printf("\t------STACK A------\n");
+		// // printf("\t------STACK A------\n");
+		// // select_hc(size, &stack);
+		// // printf("####### STACK A #######");
+		// // print_node(stack);
+		// // printf("####### STACK B #######");
+		// // print_node(stack_b);
 
-/*int	main()
-{
-
-	parseo:
-	1 que solo acepte numeros (caso contyrario debe saltar "ERROR")
-	2 los valores han de estar acotados en MAX_INT y MIN_INT
-	3 los argumentos no deben distinguir entre los que van con comillas
-	o sin comillas
-
-	push_swap();
-	return (0);
-}*/
