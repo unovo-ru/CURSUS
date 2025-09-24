4917dec345ca38eab43ef5ee6a73b5e40f6ac30e
unovo-ru <uli.nr.97@gmail.com>
2025-09-24 17:34:45 +0200

    PROYECTO PUSH SWAP TERMINADO

--- MILESTONE 2/push_swap/src_bonus/arguments_3_bonus.c
+++ MILESTONE 2/push_swap/src_bonus/arguments_3_bonus.c
@@ -6,7 +6,7 @@
 /*   By: unovo-ru <unovo-ru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 19:50:12 by unovo-ru          #+#    #+#             */
-/*   Updated: 2025/09/23 20:01:25 by unovo-ru         ###   ########.fr       */
+/*   Updated: 2025/09/24 16:39:27 by unovo-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 /*this function is the main function, it calls the rest of them*/
 
-int	*final_input(char **str)
-{
-	char	*final_argument;
-	int		*res;
+// int	*final_input(char **str)
+// {
+// 	char	*final_argument;
+// 	int		*res;
 
-	final_argument = arg_join(str);
-	res = get_imput(final_argument);
-	if (!res || !no_empty_arg(str))
-	{
-		free(res);
-		print_error();
-	}
-	free(final_argument);
-	return (res);
-}
+// 	final_argument = arg_join(str);
+// 	res = get_imput(final_argument);
+// 	if (!res || !no_empty_arg(str))
+// 	{
+// 		free(res);
+// 		print_error();
+// 	}
+// 	free(final_argument);
+// 	return (res);
+// }
