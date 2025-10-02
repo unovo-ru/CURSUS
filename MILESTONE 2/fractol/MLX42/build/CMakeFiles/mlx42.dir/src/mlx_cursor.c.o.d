38d9e0fde6a413af21952d713af885217969d727
unovo-ru <uli.nr.97@gmail.com>
2025-10-02 18:38:21 +0200

    comienzo fractol

---
+++ MILESTONE 2/fractol/MLX42/build/CMakeFiles/mlx42.dir/src/mlx_cursor.c.o.d
@@ -0,0 +1,32 @@
+CMakeFiles/mlx42.dir/src/mlx_cursor.c.o: \
+ /srv/nfs4/sgoinfre/shared/MLX42/src/mlx_cursor.c \
+ /usr/include/stdc-predef.h \
+ /srv/nfs4/sgoinfre/shared/MLX42/include/MLX42/MLX42_Int.h \
+ /srv/nfs4/sgoinfre/shared/MLX42/include/MLX42/MLX42.h \
+ /usr/lib/gcc/x86_64-redhat-linux/11/include/stddef.h \
+ /usr/lib/gcc/x86_64-redhat-linux/11/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h /usr/include/bits/wchar.h \
+ /usr/include/bits/stdint-intn.h /usr/include/bits/stdint-uintn.h \
+ /usr/lib/gcc/x86_64-redhat-linux/11/include/stdbool.h \
+ /srv/nfs4/sgoinfre/shared/MLX42/include/lodepng/lodepng.h \
+ /usr/include/string.h \
+ /srv/nfs4/sgoinfre/shared/MLX42/include/glad/glad.h \
+ /srv/nfs4/sgoinfre/shared/MLX42/include/KHR/khrplatform.h \
+ /usr/include/GLFW/glfw3.h /usr/include/stdlib.h \
+ /usr/include/bits/floatn.h /usr/include/bits/floatn-common.h \
+ /usr/include/bits/stdlib-bsearch.h /usr/include/bits/stdlib-float.h \
+ /usr/include/memory.h /usr/include/stdio.h \
+ /usr/lib/gcc/x86_64-redhat-linux/11/include/stdarg.h \
+ /usr/include/bits/types/__fpos_t.h /usr/include/bits/types/__mbstate_t.h \
+ /usr/include/bits/types/__fpos64_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/stdio.h \
+ /usr/include/linux/limits.h /usr/include/ctype.h \
+ /usr/include/bits/endian.h /usr/include/bits/endianness.h \
+ /usr/include/assert.h
