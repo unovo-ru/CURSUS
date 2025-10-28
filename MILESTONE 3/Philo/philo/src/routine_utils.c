6f4500856432063496ada87ad320f3b3d8da35e2
unovo-ru <uli.nr.97@gmail.com>
2025-10-28 15:40:58 +0100

    terminando correccion de errores

--- MILESTONE 3/Philo/philo/src/routine_utils.c
+++ MILESTONE 3/Philo/philo/src/routine_utils.c
@@ -6,7 +6,7 @@
 /*   By: unovo-ru <unovo-ru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 13:15:14 by unovo-ru          #+#    #+#             */
-/*   Updated: 2025/10/27 20:02:24 by unovo-ru         ###   ########.fr       */
+/*   Updated: 2025/10/28 11:58:00 by unovo-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,13 @@ long long	get_time(void)
 int	print_status(t_philo *philo, char *status, int force_print)
 {
 	long long	time;
-	int			lock_res;
 
-	pthread_mutex_lock(&philo->status->death_mutex);
+	pthread_mutex_lock(&philo->status->print_mutex);
 	if (philo->status->someone_died == 1 && !force_print)
 	{
-		pthread_mutex_unlock(&philo->status->death_mutex);
+		pthread_mutex_unlock(&philo->status->print_mutex);
 		return (1);
 	}
-	pthread_mutex_unlock(&philo->status->death_mutex);
-	lock_res = pthread_mutex_destroy(&philo->status->print_mutex);
-	if (lock_res)
-		return (1);
-	pthread_mutex_lock(&philo->status->print_mutex);
 	time = get_time() - philo->status->start_time;
 	printf("%lld %d %s\n", time, philo->id, status);
 	pthread_mutex_unlock(&philo->status->print_mutex);
